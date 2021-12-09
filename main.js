b7942dc3e5b0de73cc299fd0107f5e45ceda608b
Hanish565 <74765868+Hanish565@users.noreply.github.com>
2021-12-09 17:55:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 video="";
 status="";
-
+objects=[]
 function preload(){
   video=createVideo("video.mp4");
   video.hide();
@@ -13,6 +13,21 @@ function setup(){
 
 function draw(){
     image(video,0,0,620,500);
+    if (status != "") {
+        objectDetector.detect(video,gotResult);
+        for ( i = 0; i < objects.length; i++) {
+           document.getElementById("status").innerHTML="Status:Object Detected";
+           document.getElementById("objectcounter").innerHTML="The Number of objects :" + objects.length;
+           
+           fill("green");
+           percentage=floor(objects[i].confidence*100);
+           text(objects[i].label+ " " + percentage + "%",objects[i].x+15,objects[i].y+15);
+           noFill();
+           stroke("green");
+           rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+        }
+    }
 }
 
 
@@ -28,4 +43,14 @@ function modeloaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error,results){
+  if (error) {
+      console.error(error);
+  } else {
+      console.log(results);
+      objects=results;
+  }
+  
 }
